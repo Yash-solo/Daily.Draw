a63414e79054820c10be58848814704db36bc2bf
Yas-prog-blip <yashrajb916@gmail.com>
2026-06-04 16:28:00 +0530

    Update daily.js

--- daily.js
+++ daily.js
@@ -2,3 +2,6 @@ function toggleMenu() {
     const menu = document.getElementById('mobileMenu');
     menu.classList.toggle('open');
 }
+function login_website() {
+    window.location.href = 'login.html';
+}
