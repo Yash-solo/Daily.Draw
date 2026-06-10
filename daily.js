64798935aee53b55094fbeed5fb936edb6849c36
Yas-prog-blip <yashrajb916@gmail.com>
2026-06-10 14:50:53 +0530

    Add files via upload

--- daily.js
+++ daily.js
@@ -5,3 +5,19 @@ function toggleMenu() {
 function login_website() {
     window.location.href = 'login.html';
 }
+function signin_website() {
+    window.location.href = 'sign_up.html';
+}
+let slides = document.querySelectorAll('.slide')
+let current = 0;
+function Show_slide(index) {
+    slides[current].classList.remove('active');
+    current = (index + slides.length) % slides.length;
+    slides[current].classList.add('active');
+}
+function current_slide(step) {
+    Show_slide(step + current);
+}
+setInterval(() => {
+    Show_slide(current + 1);
+}, 10000);
